ea7ae8c942f5e14731bf9d9d82f254c1865c492b
BALABHASKARARAO <balabhaskararaoguntupalli@gmail.com>
2024-02-26 14:16:08 +0530

    Update ec2.tf

--- terra/ec2.tf
+++ terra/ec2.tf
@@ -19,7 +19,7 @@ resource "aws_instance" "test_cluster_m2" {
 }
 resource "aws_instance" "test_cluster_w3" {
   ami                    = data.aws_ami.base_image.id
-  instance_type          = "t2.small"
+  instance_type          = "t2.medium"
   key_name               = "terraformec2"
   vpc_security_group_ids = [aws_security_group.sec_group_block.id]
   tags = {
@@ -29,7 +29,7 @@ resource "aws_instance" "test_cluster_w3" {
 */
 resource "aws_instance" "test_cluster_w1" {
   ami                    = data.aws_ami.base_image.id
-  instance_type          = "t2.small"
+  instance_type          = "t2.medium"
   key_name               = "terraformec2"
   vpc_security_group_ids = [aws_security_group.sec_group_block.id]
   tags = {
@@ -38,7 +38,7 @@ resource "aws_instance" "test_cluster_w1" {
 }
 resource "aws_instance" "test_cluster_w2" {
   ami                    = data.aws_ami.base_image.id
-  instance_type          = "t2.small"
+  instance_type          = "t2.medium"
   key_name               = "terraformec2"
   vpc_security_group_ids = [aws_security_group.sec_group_block.id]
   tags = {
