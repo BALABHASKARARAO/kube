ab96b7a79b5416c23668b3866416a06cda5a58ff
BALABHASKARARAO <balabhaskararaoguntupalli@gmail.com>
2024-03-19 06:15:51 +0530

    Delete terra/datasources.tf

--- terra/datasources.tf
+++
@@ -1,9 +0,0 @@
-data "aws_ami" "base_image" {
-  most_recent = true
-  owners      = ["${var.datasource_ami_owner}"]
-
-  filter {
-    name   = "name"
-    values = ["${var.datasource_ami_name_filter}"]
-  }
-}
